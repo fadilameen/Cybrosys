8a0eeed9c9f6c699d6140dbef1e7ee4b2f31c8c5
Fadil Ameen <118352307+fadilameen@users.noreply.github.com>
2024-10-09 14:26:47 +0530

    Add files via upload
    workingg

--- 5currentweather/script.js
+++ 5currentweather/script.js
@@ -1,40 +1,107 @@
+const apikey = "8d856c3cb815a0a672682d3ab75a35bc";
+
 $(document).ready(function () {
   $("#getweatherbtn").click(function () {
     var city = $("#city").val();
     $.ajax({
-      url: "https://api.openweathermap.org/data/2.5/weather?q=London&appid=8d856c3cb815a0a672682d3ab75a35bc",
+      url:
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&appid=" +
+        apikey,
       method: "get",
       success: function (data) {
+        $("#error").html("");
+
         console.log(data);
         console.log(data.name);
-        $("#name").html(data.name);
+        tempk = data.main.temp;
+        tempc = tempk - 273.15;
+        tempf = (tempk * 9) / 5 - 459.67;
+        console.log(data.weather[0].description);
+        weatherdescription = data.weather[0].description;
+
+        // icon = "🌩️";
+        icon = data.weather[0].icon;
+        var url = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        $("#iconimg").attr("src", url);
+        $("#citynamedisplay").html(data.name);
+        $("#condition").html(weatherdescription);
+        $("#tempk").html(tempk + "K");
+        $("#tempc").html(tempc.toFixed(2) + "°C");
+        $("#tempf").html(tempf.toFixed(2) + "°F");
+
+        $("#lon").html(data.coord.lon);
+
+        $("#lat").html(data.coord.lat);
       },
       error: function (err) {
-        console.log(err);
+        $("#error").html("Enter a valid city");
+
+        // console.log(err);
       },
     });
   });
-
-  //   $.ajax({
-  //     url: "https://randomuser.me/api/",
-  //     method: "get",
-  //     success: function (data) {
-  //       $("#name").html(data.results[0].name.first);
-  //       $("#lname").html(data.results[0].name.last);
-  //       $("#pic").attr("src", data.results[0].picture.large);
-
-  //       console.log(data.results.length);
-  //       console.log(data.results[0]);
-
-  //       //   for (let i = 0; i < data.results.length; i++) {
-  //       //     var user = data.results[i];
-  //       // $("#name").html(data.results[0].name.first);
-
-  //       //     console.log(user.name.first);
-  //       //   }
-  //     },
-  //     error: function (err) {
-  //       console.log("errorrrrr" + err);
-  //     },
-  //   });
 });
+
+$(document).ready(function () {
+  $("#getcurrentbtn").click(function () {
+    getlocation();
+  });
+});
+
+function getlocation() {
+  navigator.geolocation.getCurrentPosition(locationdata, err);
+}
+function locationdata(pos) {
+  console.log(pos.coords.latitude);
+  console.log(pos.coords.longitude);
+  // lati = 11.25;
+  // long = 75.82;
+  lati = pos.coords.latitude.toFixed(2);
+  long = pos.coords.longitude.toFixed(2);
+  $.ajax({
+    url:
+      "https://api.openweathermap.org/data/2.5/weather?lat=" +
+      lati +
+      "&lon=" +
+      long +
+      "&appid=" +
+      apikey,
+    method: "get",
+    success: function (data) {
+      $("#error").html("");
+
+      console.log(data);
+      console.log(data.name);
+      tempk = data.main.temp;
+      tempc = tempk - 273.15;
+      tempf = (tempk * 9) / 5 - 459.67;
+      weatherdescription = data.weather[0].description;
+
+      // icon = "🌩️";
+      icon = data.weather[0].icon;
+      var url = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      $("#iconimg").attr("src", url);
+      $("#city").val(data.name);
+      $("#citynamedisplay").html(data.name);
+      $("#condition").html(weatherdescription);
+
+      $("#tempk").html(tempk + "K");
+      $("#tempc").html(tempc.toFixed(2) + "°C");
+      $("#tempf").html(tempf.toFixed(2) + "°F");
+
+      $("#lon").html(data.coord.lon);
+
+      $("#lat").html(data.coord.lat);
+    },
+    error: function (err) {
+      $("#error").html("Something went wrongg");
+
+      // console.log(err);
+    },
+  });
+}
+function err(err) {
+  console.log(err);
+}
